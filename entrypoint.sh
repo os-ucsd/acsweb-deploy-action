8aa14aa92fa9afde6b2a86cc0c55bc7440e87282
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-10-25 12:44:01 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,10 @@ ls -lah
 pwd
 
 echo $INPUT_USERNAME $INPUT_REMOTE_DIR $INPUT_LOCAL_DIR
-\
 
-sshpass StrictHostKeyChecking=no -p $INPUT_PASSWORD scp -r $INPUT_USERNAME@acsweb.ucsd.edu:~public_html/$INPUT_REMOTE_DIR $INPUT_LOCAL_DIR
+StrictHostKeyChecking=no
+
+sshpass -p $INPUT_PASSWORD scp -r $INPUT_USERNAME@acsweb.ucsd.edu:~public_html/$INPUT_REMOTE_DIR $INPUT_LOCAL_DIR
 
 echo "UCSD Personal Site Deploy complete"
 exit 0
