59dce058e7fadf662b1e35275a0f5f4c9ab6a71a
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-10-30 11:18:07 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,9 @@
 # "to avoid continuing when errors or undefined variables are present"
 set -eu
 
-echo "starting..."
-
-ls -lah
-
-pwd
-
-echo 1, $INPUT_USERNAME, 2, $INPUT_REMOTE_DIR, 3, $INPUT_LOCAL_DIR
-
+echo "Starting a UCSD ACSWeb Deploy to $INPUT_USERNAME acsweb.ucsd.edu... "
 
 sshpass -p $INPUT_PASSWORD rsync -vzr --delete --exclude '.*'  -e 'ssh -o StrictHostKeyChecking=no' $INPUT_LOCAL_DIR $INPUT_USERNAME@acsweb.ucsd.edu:~/public_html/$INPUT_REMOTE_DIR
 
-echo "UCSD Personal Site Deploy complete"
+echo "UCSD ACSWeb Deploy complete! 🚀"
 exit 0
